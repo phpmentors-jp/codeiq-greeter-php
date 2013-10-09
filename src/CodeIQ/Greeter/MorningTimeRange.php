6e36bdbc3466df15e73d2da923119ec48e21bed7
hidenorigoto <hidenorigoto@gmail.com>
2013-10-09 23:31:50 +0900

    [MorningTimeRange] contains()を実装した。

--- src/CodeIQ/Greeter/MorningTimeRange.php
+++ src/CodeIQ/Greeter/MorningTimeRange.php
@@ -6,6 +6,7 @@ class MorningTimeRange
 {
     public function contains(\DateTimeImmutable $target)
     {
-        return true;
+        return $target >= new \DateTimeImmutable('05:00:00') &&
+        $target < new \DateTimeImmutable('12:00:00');
     }
 }
