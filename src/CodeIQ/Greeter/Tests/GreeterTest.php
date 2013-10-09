c74bf52f878c49d5cafb12983fc5b4c81db5ecbd
hidenorigoto <hidenorigoto@gmail.com>
2013-10-10 04:41:38 +0900

    [Tests/GreeterTest] dataProviderを使ってロケールパラメータの可変についてもテストできるようにした

--- src/CodeIQ/Greeter/Tests/GreeterTest.php
+++ src/CodeIQ/Greeter/Tests/GreeterTest.php
@@ -22,8 +22,9 @@ class GreeterTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
+     * @dataProvider ロケールごとのあいさつデータ
      */
-    public function 最初の時間範囲にマッチ()
+    public function 最初の時間範囲にマッチ($locale, $one, $two, $three)
     {
         $time = new \DateTimeImmutable();
 
@@ -31,26 +32,40 @@ class GreeterTest extends \PHPUnit_Framework_TestCase
             ->method('getCurrentTime')
             ->will($this->returnValue($time));
 
+        $this->globe->expects($this->once())
+            ->method('getLocale')
+            ->will($this->returnValue($locale));
+
         $firstTimeRange = $this->getMock('CodeIQ\Greeter\TimeRange', array(), array(), '', false);
         $firstTimeRange->expects($this->once())
             ->method('contains')
             ->with($this->equalTo($time))
             ->will($this->returnValue(true));
+
+        $firstTimeRange->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue('firstrange'));
 
         $secondTimeRange = $this->getMock('CodeIQ\Greeter\TimeRange', array(), array(), '', false);
         $secondTimeRange->expects($this->never())
             ->method('contains');
+        $secondTimeRange->expects($this->never())
+            ->method('getId');
 
-        $this->SUT->addTimeRangeAndGreeting($firstTimeRange, 'one');
-        $this->SUT->addTimeRangeAndGreeting($secondTimeRange, 'two');
+        $this->SUT->addTimeRange($firstTimeRange);
+        $this->SUT->addTimeRange($secondTimeRange);
+        $this->SUT->addGreeting($locale, 'firstrange', $one);
+        $this->SUT->addGreeting($locale, 'secondrange', $two);
+        $this->SUT->addGreeting($locale, 'thirdrange', $three);
 
-        $this->assertThat($this->SUT->greet(), $this->equalTo('one'));
+        $this->assertThat($this->SUT->greet(), $this->equalTo($one));
     }
 
     /**
      * @test
+     * @dataProvider ロケールごとのあいさつデータ
      */
-    public function ニつ目の時間範囲にマッチ()
+    public function ニつ目の時間範囲にマッチ($locale, $one, $two, $three)
     {
         $time = new \DateTimeImmutable();
 
@@ -58,11 +73,18 @@ class GreeterTest extends \PHPUnit_Framework_TestCase
             ->method('getCurrentTime')
             ->will($this->returnValue($time));
 
+        $this->globe->expects($this->once())
+            ->method('getLocale')
+            ->will($this->returnValue($locale));
+
         $firstTimeRange = $this->getMock('CodeIQ\Greeter\TimeRange', array(), array(), '', false);
         $firstTimeRange->expects($this->once())
             ->method('contains')
             ->with($this->equalTo($time))
             ->will($this->returnValue(false));
+
+        $firstTimeRange->expects($this->never())
+            ->method('getId');
 
         $secondTimeRange = $this->getMock('CodeIQ\Greeter\TimeRange', array(), array(), '', false);
         $secondTimeRange->expects($this->once())
@@ -70,21 +92,31 @@ class GreeterTest extends \PHPUnit_Framework_TestCase
             ->with($this->equalTo($time))
             ->will($this->returnValue(true));
 
+        $secondTimeRange->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue('secondrange'));
+
         $thirdTimeRange = $this->getMock('CodeIQ\Greeter\TimeRange', array(), array(), '', false);
         $thirdTimeRange->expects($this->never())
             ->method('contains');
+        $thirdTimeRange->expects($this->never())
+            ->method('getId');
 
-        $this->SUT->addTimeRangeAndGreeting($firstTimeRange, 'one');
-        $this->SUT->addTimeRangeAndGreeting($secondTimeRange, 'two');
-        $this->SUT->addTimeRangeAndGreeting($thirdTimeRange, 'three');
+        $this->SUT->addTimeRange($firstTimeRange);
+        $this->SUT->addTimeRange($secondTimeRange);
+        $this->SUT->addTimeRange($thirdTimeRange);
+        $this->SUT->addGreeting($locale, 'firstrange', $one);
+        $this->SUT->addGreeting($locale, 'secondrange', $two);
+        $this->SUT->addGreeting($locale, 'thirdrange', $three);
 
-        $this->assertThat($this->SUT->greet(), $this->equalTo('two'));
+        $this->assertThat($this->SUT->greet(), $this->equalTo($two));
     }
 
     /**
      * @test
+     * @dataProvider ロケールごとのあいさつデータ
      */
-    public function 時間範囲にマッチしない()
+    public function 時間範囲にマッチしない($locale, $one, $two, $three)
     {
         $time = new \DateTimeImmutable();
 
@@ -98,16 +130,32 @@ class GreeterTest extends \PHPUnit_Framework_TestCase
             ->with($this->equalTo($time))
             ->will($this->returnValue(false));
 
+        $firstTimeRange->expects($this->never())
+            ->method('getId');
+
         $secondTimeRange = $this->getMock('CodeIQ\Greeter\TimeRange', array(), array(), '', false);
         $secondTimeRange->expects($this->once())
             ->method('contains')
             ->with($this->equalTo($time))
             ->will($this->returnValue(false));
 
-        $this->SUT->addTimeRangeAndGreeting($firstTimeRange, 'one');
-        $this->SUT->addTimeRangeAndGreeting($secondTimeRange, 'two');
+        $secondTimeRange->expects($this->never())
+            ->method('getId');
+
+        $this->SUT->addTimeRange($firstTimeRange);
+        $this->SUT->addTimeRange($secondTimeRange);
+        $this->SUT->addGreeting($locale, 'firstrange', $one);
+        $this->SUT->addGreeting($locale, 'secondrange', $two);
 
         $this->assertThat($this->SUT->greet(), $this->equalTo(''));
+    }
+
+    public function ロケールごとのあいさつデータ()
+    {
+        return [
+            ['ja', 'おはようございます', 'こんにちは', 'こんばんは'],
+            ['en', 'Good Morning', 'Good afternoon', 'Good evening'],
+        ];
     }
 
     protected function setUp()
